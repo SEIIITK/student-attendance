f1e9ad0a9c0e214a4739814bbafb48e52db37a8e
rahul <=rahulrajyadav2014@gmail.com>
2018-03-30 21:55:38 +0530

    Error debugged

--- getvalues.php
+++ getvalues.php
@@ -3,14 +3,15 @@
 $mysql_host = 'localhost';
 $mysql_user = 'root';
 $mysql_password = '';
-$mysql_databse = 'Student_Atteandance';
+$mysql_databse = 'Student_Attendance';
 
 $con = mysqli_connect($mysql_host,$mysql_user,$mysql_password,$mysql_databse) or die("cannt connect");
 //echo"connection successfull".'<br>';
 
 if(isset($_POST['submit']))
 {
-echo $id = $_POST['id'];
+
+ $id = $_POST['id'];
 $name =  $_POST['name'];
 $department =  $_POST['department'];
 $year =  $_POST['year'];
@@ -21,15 +22,15 @@ $email =  $_POST['email'];
 $mobile_no =  $_POST['mobile_no'];
 $address =  $_POST['address'];
 
-$sql = "insert into Add_Student(id,name,department,year,semester,roll_no,reg_no,email,mobile_no,address)values('$id','$name','$department',
-                               '$year','$semester','$roll_no','$reg_no','$email','$mobile_no','$address')";
-if (mysqli_query($conn, $sql)) 
+$sql = "insert into Add_Student(Id,Name,Department,Year,Semester,Roll_no,Reg_no,Email_address,Mobile_no,Address) values('$id','$name','$department','$year','$semester','$roll_no','$reg_no','$email','$mobile_no','$address')";
+
+if (mysqli_query($con, $sql)) 
  {
     echo "New record created successfully";
 } 
 else
 {
-    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    echo "Error: ".$sql."<br>".mysqli_error($conn);
 }
 
 
